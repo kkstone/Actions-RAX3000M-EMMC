6f80b466cbc363a5628cfa8298d95cffdeeaf4fc
topstarxxx <topstarxxx@gmail.com>
2025-02-14 22:52:10 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -5,9 +5,9 @@
 cd package
 git clone https://github.com/xiaorouji/openwrt-passwall.git
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git
-git clone https://github.com/kuoruan/openwrt-frp.git
-git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
+# git clone https://github.com/kuoruan/openwrt-frp.git
+# git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 
-wget https://github.com/kkstone/Actions-RAX3000M-NAND-EMMC/raw/emmc/luci-packages/luci-app-frp.zip && unzip luci-app-frp.zip && rm -rf luci-app-frp.zip
+# wget https://github.com/kkstone/Actions-RAX3000M-NAND-EMMC/raw/emmc/luci-packages/luci-app-frp.zip && unzip luci-app-frp.zip && rm -rf luci-app-frp.zip
 wget https://github.com/kkstone/Actions-RAX3000M-NAND-EMMC/raw/emmc/luci-packages/luci-app-wolplus.zip && unzip luci-app-wolplus.zip && rm -rf luci-app-wolplus.zip
