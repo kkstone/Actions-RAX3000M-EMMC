0e3cc2b4a28fae38e397ed0effe6e49101ba7603
kkstone <10545609+kkstone@users.noreply.github.com>
2025-06-04 19:47:29 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -3,7 +3,7 @@
 # Add a feed source
 
 cd package
-rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,frp,smartdns,zerotier,natmap,ddns-go}
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,frp,smartdns,zerotier,natmap,ddns-go,tcping}
 rm -rf feeds/luci/luci-app-frp*
 rm -rf feeds/packages/utils/{ttyd,7z}
 
@@ -14,7 +14,6 @@ git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/mwarning/zerotier-openwrt.git
 git clone https://github.com/pymumu/openwrt-smartdns.git
-wget -N -O feeds/packages/net/tcping/Makefile https://cdn.jsdelivr.net/gh/immortalwrt/packages@master/net/tcping/Makefile
 wget -r -np -nH --cut-dirs=4 -P feeds/packages/net/ https://cdn.jsdelivr.net/gh/immortalwrt/packages@master/net/tcping/ && find feeds/packages/net/tcping -type f -name "index.html*" -exec rm {} \;
 wget -r -np -nH --cut-dirs=4 -P feeds/packages/net/ https://cdn.jsdelivr.net/gh/immortalwrt/packages@master/net/natmap/ && find feeds/packages/net/natmap -type f -name "index.html*" -exec rm {} \;
 wget -r -np -nH --cut-dirs=4 -P feeds/packages/net/ https://cdn.jsdelivr.net/gh/immortalwrt/packages@master/net/ddns-go/ && find feeds/packages/net/ddns-go -type f -name "index.html*" -exec rm {} \;
