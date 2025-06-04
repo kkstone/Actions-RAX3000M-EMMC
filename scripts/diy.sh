b379341be270a873ea364b9f1da7779b9bfbc011
kkstone <10545609+kkstone@users.noreply.github.com>
2025-06-04 11:58:43 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -7,6 +7,8 @@
 sed -i "s,mirrors.vsean.net/openwrt,mirrors.sjtug.sjtu.edu.cn/immortalwrt,g" package/emortal/default-settings/files/99-default-settings-chinese
 
 #添加kmod-ipt-socket等Passwall必须模块
+rm -rf $GITHUB_WORKSPACE/openwrt/package/emortal/autocore/files/generic/21_ethinfo.js
+cp -rf $GITHUB_WORKSPACE/patchs/21_ethinfo.js $GITHUB_WORKSPACE/openwrt/package/emortal/autocore/files/generic/21_ethinfo.js
 cp -rf $GITHUB_WORKSPACE/patchs/iptables-makefile.patch $GITHUB_WORKSPACE/openwrt/p1.patch
 patch -p1 < $GITHUB_WORKSPACE/openwrt/p1.patch
 cp -rf $GITHUB_WORKSPACE/patchs/netfilter.patch $GITHUB_WORKSPACE/openwrt/p2.patch
