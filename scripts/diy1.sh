37c9280093b7a61a83e553c7bb7c95d0722f8b0a
kkstone <10545609+kkstone@users.noreply.github.com>
2025-06-04 20:42:27 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -5,9 +5,13 @@
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,frp,smartdns,zerotier,natmap,ddns-go,tcping}
 rm -rf feeds/luci/luci-app-frp*
 rm -rf feeds/packages/utils/{ttyd,7z}
+wget -r -np -nH --cut-dirs=4 -P feeds/packages/net/ https://cdn.jsdelivr.net/gh/immortalwrt/packages@master/net/tcping/ && find feeds/packages/net/tcping -type f -name "index.html*" -exec rm {} \;
+wget -r -np -nH --cut-dirs=4 -P feeds/packages/net/ https://cdn.jsdelivr.net/gh/immortalwrt/packages@master/net/natmap/ && find feeds/packages/net/natmap -type f -name "index.html*" -exec rm {} \;
+wget -r -np -nH --cut-dirs=4 -P feeds/packages/net/ https://cdn.jsdelivr.net/gh/immortalwrt/packages@master/net/ddns-go/ && find feeds/packages/net/ddns-go -type f -name "index.html*" -exec rm {} \;
+wget -r -np -nH --cut-dirs=4 -P feeds/packages/utils/ https://cdn.jsdelivr.net/gh/immortalwrt/packages@master/utils/ttyd/ && find feeds/packages/utils/ttyd -type f -name "index.html*" -exec rm {} \;
+wget -r -np -nH --cut-dirs=4 -P feeds/packages/utils/ https://cdn.jsdelivr.net/gh/immortalwrt/packages@master/utils/7z/ && find feeds/packages/utils/7z -type f -name "index.html*" -exec rm {} \;
 
 cd package
-
 git clone https://github.com/xiaorouji/openwrt-passwall.git
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git
 git clone https://github.com/kuoruan/openwrt-frp.git
@@ -15,12 +19,6 @@ git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/mwarning/zerotier-openwrt.git
 git clone https://github.com/pymumu/openwrt-smartdns.git
-wget -r -np -nH --cut-dirs=4 -P feeds/packages/net/ https://cdn.jsdelivr.net/gh/immortalwrt/packages@master/net/tcping/ && find feeds/packages/net/tcping -type f -name "index.html*" -exec rm {} \;
-wget -r -np -nH --cut-dirs=4 -P feeds/packages/net/ https://cdn.jsdelivr.net/gh/immortalwrt/packages@master/net/natmap/ && find feeds/packages/net/natmap -type f -name "index.html*" -exec rm {} \;
-wget -r -np -nH --cut-dirs=4 -P feeds/packages/net/ https://cdn.jsdelivr.net/gh/immortalwrt/packages@master/net/ddns-go/ && find feeds/packages/net/ddns-go -type f -name "index.html*" -exec rm {} \;
-wget -r -np -nH --cut-dirs=4 -P feeds/packages/utils/ https://cdn.jsdelivr.net/gh/immortalwrt/packages@master/utils/ttyd/ && find feeds/packages/utils/ttyd -type f -name "index.html*" -exec rm {} \;
-wget -r -np -nH --cut-dirs=4 -P feeds/packages/utils/ https://cdn.jsdelivr.net/gh/immortalwrt/packages@master/utils/7z/ && find feeds/packages/utils/7z -type f -name "index.html*" -exec rm {} \;
-
 
 unzip -d ./ $GITHUB_WORKSPACE/luci-packages/luci-app-frp.zip
 unzip -d ./ $GITHUB_WORKSPACE/luci-packages/luci-app-wolplus.zip
