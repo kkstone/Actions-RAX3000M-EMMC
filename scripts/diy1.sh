b69f2fd349d37a7b350ae822537a2db005022136
kkstone <10545609+kkstone@users.noreply.github.com>
2025-12-26 09:14:39 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -7,7 +7,7 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git && rm -rf openwrt-passwall-packages/tcping
 git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/kuoruan/openwrt-frp.git -b releases/v0.63.0-1
+git clone https://github.com/kuoruan/openwrt-frp.git -b releases/v0.65.0-1
 git clone https://github.com/mwarning/zerotier-openwrt.git && rm -rf zerotier-openwrt/zerotier/files/etc/init.d/zerotier
 git clone https://github.com/kkstone/7zz-openwrt.git
 
